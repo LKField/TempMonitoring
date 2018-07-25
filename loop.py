542b1223c754c15167133995ce108256f93e831e
Lucretia Field <40864514+LKField@users.noreply.github.com>
2018-07-25 10:10:48 -0400

    Add files via upload
    Looping code to check through the data table, print first column values, and stop at empty cells
    Problem: Prints each cell value 3 times

---
+++ loop.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import matplotlib as plt
+import numpy as np
+from datetime import datetime, timedelta as dt
+import xlrd
+
+# define empty_cell boolean
+empty_cell= False
+with xlrd.open_workbook(filename='Demo1_13.xlsx') as wb:
+    cs= wb.sheet_by_index(0)
+    num_cols= cs.ncols
+    num_rows= cs.nrows
+    # set count empty cells
+    count_full = 0
+    count_empty = 0
+    for row_index in range(1, num_rows):
+        for col_index in range(0,num_cols):
+            # get cell value
+            cell_val= cs.cell(row_index, col_index).value
+            # check if cell is empty
+            if cell_val== '':
+                # set empty cell is True
+                empty_cell = True
+                # increment counter
+                count_empty+= 1
+            else:
+                # set empty cell is false (cell has value)
+                empty_cell= False
+                # increment counter based on the row index (assumes no empty cells within data block)
+                count_full= row_index
+                print(count_full)
+            # check if cell is not empty
+            if not empty_cell:
+                # print value of cell as a datetime
+                date = cs.cell_value(count_full,0)
+                datetime_value = datetime(*xlrd.xldate_as_tuple(date, 0))
+                print(datetime_value)
+                continue
+        # check the counter if is = num_cols means the whole row is empty
+        if count_empty == num_cols:
+            # stop looping to next rows
+            break
