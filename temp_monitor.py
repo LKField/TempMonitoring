e6f1a128a4c2737f4fc264c42a481bea58393c34
Lucretia Field <40864514+LKField@users.noreply.github.com>
2018-07-26 10:45:14 -0400

    Add files via upload
    Fixed: sorting the data, add comments
    To Fix: x-axis time stamp correct values

--- temp_monitor.py
+++ temp_monitor.py
@@ -1,3 +1,8 @@
+# Temperature/Humidity analysis program to read in data from a spreadsheet
+# Plot the data in a static HTML output file
+# Author: Lucretia Field
+# Date: July 26, 2018
+
 from bokeh.plotting import figure, output_file, show
 import bokeh.layouts
 import pandas as pd
@@ -8,14 +13,15 @@ import xlrd
 
 # Ask user for input names of file (later create a script or other mechanism to automate this)
 my_input = ''
-my_data = pd.DataFrame()
+my_data = pd.DataFrame() # create empty data frame to populate with read in values
 while my_input != 'exit':
     my_input = input("Write filename to read or type 'exit' to end: ")
     if my_input != 'exit':
-        my_file = pd.read_excel(my_input, parse_dates=['Timestamp for sample frequency every 15 min'])
-        my_file.set_index(keys='Timestamp for sample frequency every 15 min', drop=True, inplace=True)
-        my_data = my_data.append(my_file)
-        my_data.drop_duplicates(keep=False)
+        my_file = pd.read_excel(my_input, parse_dates=['Timestamp for sample frequency every 15 min']) # read in data
+        my_file.set_index(keys='Timestamp for sample frequency every 15 min', drop=True, inplace=True) # set time as index
+        my_data = my_data.append(my_file) # append the data to dataframe 'my_data'
+        my_data = my_data.drop_duplicates(keep=False) # delete duplicates if any are present
+        my_data = my_data.sort_values(['Timestamp for sample frequency every 15 min'], ascending=[True]) # sort the data
     else:
         break
 
