e495355b4bd7a7b4427f6ccb52f14c5b50f7bbe9
Lucretia Field <40864514+LKField@users.noreply.github.com>
2018-07-26 10:28:38 -0400

    Add files via upload
    Fixed: appending and index names as dates: 
    To Fix: sort data by date, correct format for time

--- temp_monitor.py
+++ temp_monitor.py
@@ -3,26 +3,25 @@ import bokeh.layouts
 import pandas as pd
 import matplotlib as plt
 import numpy as np
-import datetime
+from datetime import datetime, timedelta as dt
+import xlrd
 
-#Ask user for input names of file (later create a script or other mechanism to automate this)
+# Ask user for input names of file (later create a script or other mechanism to automate this)
 my_input = ''
-my_data = []
+my_data = pd.DataFrame()
 while my_input != 'exit':
     my_input = input("Write filename to read or type 'exit' to end: ")
     if my_input != 'exit':
         my_file = pd.read_excel(my_input, parse_dates=['Timestamp for sample frequency every 15 min'])
-        print(my_file)
-        my_data = my_file.append(my_file)
-        continue
-    if my_input == 'exit':
-        my_data.sort_values(by='Timestamp for sample frequency every 15 min')
-        print(my_data)
+        my_file.set_index(keys='Timestamp for sample frequency every 15 min', drop=True, inplace=True)
+        my_data = my_data.append(my_file)
+        my_data.drop_duplicates(keep=False)
+    else:
         break
 
-time = my_data[my_data.columns[0]]
-temp = my_data[my_data.columns[1]]
-humidity = my_data[my_data.columns[2]]
+print(my_data)
+temp = my_data[my_data.columns[0]]
+humidity = my_data[my_data.columns[1]]
 
 # output to static HTML file
 output_file("output.html")
@@ -32,7 +31,7 @@ p = figure(title="Temperature over Time in Demo Lab 1", x_axis_label='Time (ns)'
 p.sizing_mode='scale_height'
 
 # add a line renderer with legend and line thickness
-p.line(humidity, temp, legend="Temp.", line_width=2)
+p.line(x=my_data.index.values, y=temp, legend="Temp.", line_width=2)
 
 # show the results
 show(p)
