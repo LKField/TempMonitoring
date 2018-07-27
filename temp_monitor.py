bb04a0ae13e5a25fbfd27b313aea0accce1c978c
Lucretia Field <40864514+LKField@users.noreply.github.com>
2018-07-27 10:16:49 -0400

    Add files via upload
    Fixed: Data drops/mistakes, added Humidity graph 
    To Add: Link temp and humidity

--- temp_monitor.py
+++ temp_monitor.py
@@ -5,6 +5,7 @@
 
 from bokeh.plotting import figure, output_file, show
 import bokeh.layouts
+from bokeh.layouts import row
 import pandas as pd
 import matplotlib as plt
 import numpy as np
@@ -14,14 +15,16 @@ import xlrd
 # Ask user for input names of file (later create a script or other mechanism to automate this)
 my_input = ''
 my_data = pd.DataFrame() # create empty data frame to populate with read in values
+print(my_data)
 while my_input != 'exit':
     my_input = input("Write filename to read or type 'exit' to end: ")
     if my_input != 'exit':
         my_file = pd.read_excel(my_input, parse_dates=['Timestamp for sample frequency every 15 min']) # read in data
+        print(len(my_file))
         my_file.set_index(keys='Timestamp for sample frequency every 15 min', drop=True, inplace=True) # set time as index
         my_data = my_data.append(my_file) # append the data to dataframe 'my_data'
-        my_data = my_data.drop_duplicates(keep='first') # delete duplicates if any are present
         my_data = my_data.sort_values(['Timestamp for sample frequency every 15 min'], ascending=True) # sort the data
+        print(len(my_data))
     else:
         break
 
@@ -33,13 +36,17 @@ humidity = my_data[my_data.columns[1]]
 output_file("output.html")
 
 # create a new plot with a title and axis labels
-p = figure(title="Temperature over Time in Demo Lab 1", x_axis_type='datetime', x_axis_label='Date and Time', y_axis_label='Temperature (deg F)')
-p.xaxis[0].ticker.desired_num_ticks = 30
-p.yaxis[0].ticker.desired_num_ticks = 30
-p.sizing_mode='scale_height'
+p1 = figure(title="Temperature over Time in Demo Lab 1", plot_width=800, x_axis_type='datetime', x_axis_label='Date and Time', y_axis_label='Temperature (deg F)')
+p1.xaxis[0].ticker.desired_num_ticks = 20
+p1.yaxis[0].ticker.desired_num_ticks = 20
+
+p2 = figure(title="Humidity over Time in Demo Lab 1", plot_width=800, x_axis_type='datetime', x_axis_label='Date and Time', y_axis_label='Humidity (%)')
+p2.xaxis[0].ticker.desired_num_ticks = 20
+p2.yaxis[0].ticker.desired_num_ticks = 20
 
 # add a line renderer with legend and line thickness
-p.line(x=my_data.index, y=temp, legend="Temp.", line_width=2)
+p1.line(x=my_data.index, y=temp, line_width=2)
+p2.line(x=my_data.index, y=humidity, line_width=2)
 
 # show the results
-show(p)
+show(row(p1, p2))
