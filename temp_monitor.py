bfb15a4ffb567f36ea6efec3b212abd576db4531
Lucretia Field <40864514+LKField@users.noreply.github.com>
2018-07-27 08:38:31 -0400

    Add files via upload
    Fixed: Date format in axis 
    To Fix: Errors in removing duplicates which changes the graph when data entered in certain ways

--- temp_monitor.py
+++ temp_monitor.py
@@ -20,8 +20,8 @@ while my_input != 'exit':
         my_file = pd.read_excel(my_input, parse_dates=['Timestamp for sample frequency every 15 min']) # read in data
         my_file.set_index(keys='Timestamp for sample frequency every 15 min', drop=True, inplace=True) # set time as index
         my_data = my_data.append(my_file) # append the data to dataframe 'my_data'
-        my_data = my_data.drop_duplicates(keep=False) # delete duplicates if any are present
-        my_data = my_data.sort_values(['Timestamp for sample frequency every 15 min'], ascending=[True]) # sort the data
+        my_data = my_data.drop_duplicates(keep='first') # delete duplicates if any are present
+        my_data = my_data.sort_values(['Timestamp for sample frequency every 15 min'], ascending=True) # sort the data
     else:
         break
 
@@ -33,11 +33,11 @@ humidity = my_data[my_data.columns[1]]
 output_file("output.html")
 
 # create a new plot with a title and axis labels
-p = figure(title="Temperature over Time in Demo Lab 1", x_axis_label='Time (ns)', y_axis_label='Temperature (deg F)')
+p = figure(title="Temperature over Time in Demo Lab 1", x_axis_type='datetime', x_axis_label='Time', y_axis_label='Temperature (deg F)')
 p.sizing_mode='scale_height'
 
 # add a line renderer with legend and line thickness
-p.line(x=my_data.index.values, y=temp, legend="Temp.", line_width=2)
+p.line(x=my_data.index, y=temp, legend="Temp.", line_width=2)
 
 # show the results
 show(p)
