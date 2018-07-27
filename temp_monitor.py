9ab97c73cd0f9b02de0dd7d0dd3174e40d0d4543
Lucretia Field <40864514+LKField@users.noreply.github.com>
2018-07-27 11:19:26 -0400

    Add files via upload
    Fixed: automatically find all data in user indicated folder 
    To Fix: link temp and humidity, add pointers

--- temp_monitor.py
+++ temp_monitor.py
@@ -1,7 +1,7 @@
 # Temperature/Humidity analysis program to read in data from a spreadsheet
 # Plot the data in a static HTML output file
 # Author: Lucretia Field
-# Date: July 26, 2018
+# Date: July 2018
 
 from bokeh.plotting import figure, output_file, show
 import bokeh.layouts
@@ -12,37 +12,27 @@ import numpy as np
 from datetime import datetime, timedelta as dt
 from pathlib import Path
 
-pathlist = Path('Demo1').glob('**/*.csv')
+my_input = input('Enter folder name or exit: ') # ask user to pick folder or exit
+pathlist = Path(my_input).glob('**/*.csv') # gather all files in folder
 for path in pathlist:
-    # because path is object not string
     path_in_str = str(path)
-    print(path_in_str)
 
-# Ask user for input names of file (later create a script or other mechanism to automate this)
-my_input = ''
 my_data = pd.DataFrame() # create empty data frame to populate with read in values
-print(my_data)
-while my_input != 'exit':
-    my_input = path_in_str
-    if my_input != 'exit':
-        my_file = pd.read_csv(my_input, parse_dates=['Timestamp for sample frequency every 15 min']) # read in data
-#        print(len(my_file))
-        my_file.set_index(keys='Timestamp for sample frequency every 15 min', drop=True, inplace=True) # set time as index
-        my_data = my_data.append(my_file) # append the data to dataframe 'my_data'
-        my_data = my_data.sort_values(['Timestamp for sample frequency every 15 min'], ascending=True) # sort the data
-#        print(len(my_data))
-    else:
-        break
+my_file = pd.read_csv(path_in_str, parse_dates=['Timestamp for sample frequency every 15 min']) # read in data from chosen folder
+my_file.set_index(keys='Timestamp for sample frequency every 15 min', drop=True, inplace=True) # set time as index
+my_data = my_data.append(my_file) # append the data to dataframe 'my_data'
+my_data = my_data.sort_values(['Timestamp for sample frequency every 15 min'], ascending=True) # sort the data
 
+# define temperature and humidity data
 temp = my_data[my_data.columns[0]]
 humidity = my_data[my_data.columns[1]]
 
 # output to static HTML file
 output_file("output.html")
 
-# create a new plot with a title and axis labels
+# create two new plots with a title and axis labels
 p1 = figure(title="Temperature over Time in Demo Lab 1", plot_width=800, x_axis_type='datetime', x_axis_label='Date and Time', y_axis_label='Temperature (deg F)')
-p1.xaxis[0].ticker.desired_num_ticks = 20
+p1.xaxis[0].ticker.desired_num_ticks = 20 #set number of marks in graphs
 p1.yaxis[0].ticker.desired_num_ticks = 20
 
 p2 = figure(title="Humidity over Time in Demo Lab 1", plot_width=800, x_axis_type='datetime', x_axis_label='Date and Time', y_axis_label='Humidity (%)')
