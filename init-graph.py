aaa5c1956de3f1f9c11f5eb788a8b010ffdf5667
Lucretia Field <40864514+LKField@users.noreply.github.com>
2018-12-05 15:41:02 -0500

    Add files via upload
    init-graph.py now takes in all .csv files in the same folder and sorts and graphs them

--- init-graph.py
+++ init-graph.py
@@ -4,34 +4,70 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 import csv
+import glob
 
-with open('Test.csv') as csvfile:
-    my_data = csv.reader(csvfile) # read in csv file
-    next(my_data, None)
-    dates = []
-    temps = []
-    humids = []
+# create a file list of all the .csv files in the given folder
+file_list = glob.glob("*.csv")
+print(file_list)
 
-    for row in my_data: # read in and format rows
-        date = row[0]
-        temp = row[1]
-        humid = row[2]
+dates = []
+temps = []
+humids = []
 
-        dates.append(date)
-        temps.append(temp)
-        humids.append(humid)
+# open all the .csv files and create separated data lists
+for name in file_list:
+    print("After 'for' Opening:", name)
+    with open(name) as csvfile:
+        my_data = csv.reader(csvfile) # read in csv file
+        next(my_data, None)
 
-        form_dates = pd.to_datetime(dates) # convert column 1 to datetime
+        for row in my_data: # read in and format rows
+            date = row[0]
+            temp = row[1]
+            humid = row[2]
 
-#    print(form_dates)
-#    print(temps)
-#    print(humids)
+            dates.append(date)
+            temps.append(temp)
+            humids.append(humid)
 
-trace1 = go.Scatter(x=form_dates, y=temps, name='Temperature (F)')
-trace2 = go.Scatter(x=form_dates, y=humids, name='Relative Humidity (%)')
+            form_dates = pd.to_datetime(dates) # convert column 1 to datetime
 
+#print(form_dates)
+#print(temps)
+#print(humids)
+df = pd.DataFrame({"date": form_dates, "temp": temps, "humid": humids})
+df = df.sort_values(by=['date'])
 
+# create traces to graph from gathered data
+trace1 = go.Scatter(x=df.date, y=df.temp, name='Temperature (F)')
+trace2 = go.Scatter(x=df.date, y=df.humid, name='Relative Humidity (%)')
+
+# graph the data using Dash in two separate graphs
 app = dash.Dash()
+
+# creates a dropdpown menu for later data selection, currently not in use
+select = html.Div([
+    dcc.Dropdown(
+    id='select',
+    options=[ {'label': x, 'value': x} for x in file_list]
+    )
+    ],
+    style={'width' : '25%',
+            'display' : 'inline-block'}
+)
+# creates initialization of graphs, not currently in use
+graph = dcc.Graph(
+    id='temperature-graph',
+    figure={
+    'data':[{}]
+    }
+)
+graph = dcc.Graph(
+    id='humidity-graph',
+    figure={
+    'data':[{}]
+    }
+)
 app.layout = html.Div(children=[
     html.H1(children='Demo Lab 1 Norwood Temperature and Humidity Data'),
     dcc.Graph(
